b5dad9177acfb35a41a304c61add1a53a2db24c8
LunZaiZai0223 <liu.wei.lun0223@gmail.com>
2023-05-09 23:39:38 +0800

    complete day 1 challenge

---
+++ Day 1 - 2667. Create Hello World Function/index.js
@@ -0,0 +1,16 @@
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+	return function(...args) {
+		return 'Hello World'
+	}
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
+
+
+// [2667. Create Hello World Function](https://leetcode.com/problems/create-hello-world-function/description/?utm_campaign=PostD1&utm_medium=Post&utm_source=Post&gio_link_id=QPDw0kJR)
